b145bef47512882eff48538c3746974fe1622c69
Mohd <itssmohammed@gmail.com>
2020-05-15 05:45:10 +0300

    updates

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
 
-
-
-
 // Slide Menu
 
-    function openSlideMenu() {
-        document.getElementById('side-menu').style.width = '250px';
+function openSlideMenu() {
+    document.getElementById('sideMenu').style.width = '250px';
+    document.querySelector('.menuIcon').style.transform = 'rotate(90deg)';
 
 }
 
-    function closeSlideMenu(){
-        document.getElementById('side-menu').style.width = '0';
-                document.getElementById('side-menu').onclick = function(e) {
-            e.preventDefault();
-            return false;
-        };
-
+function closeSlideMenu(){
+    document.getElementById('sideMenu').style.width = '0';
+    document.querySelector('.menuIcon').style.transform = '';
     }
+
+window.onscroll = function () {
+const nav = document.querySelector('.navBar');
+
+  if(window.pageYOffset > 43){
+
+   nav.style.position = "fixed";
+   nav.style.backgroundColor = "black";
+
+   } else{
+     nav.style.position = "absolute";
+     nav.style.backgroundColor = "";
+   }
+}
+
+
